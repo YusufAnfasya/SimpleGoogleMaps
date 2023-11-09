3a47516a3ca12367f27ee77e20fb6e7872bd646d
YusufAnfasya <80965895+YusufAnfasya@users.noreply.github.com>
2023-11-09 16:44:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/simplegooglemaps/MapsActivity.java
@@ -0,0 +1,143 @@
+package com.example.simplegooglemaps;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.simplegooglemaps.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button go = (Button) findViewById(R.id.idGo);
+        go.setOnClickListener(op);
+
+        Button cari = (Button) findViewById(R.id.idCari);
+        cari.setOnClickListener(op);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng its = new LatLng(-7.28, 112.79);
+        mMap.addMarker(new MarkerOptions().position(its).title("Marker in ITS"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(its, 8));
+    }
+
+    View.OnClickListener op = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            if (view.getId() == R.id.idGo) {
+                gotoLokasi();
+                sembunyikanKeyBoard(view);
+            } else if (view.getId() == R.id.idCari) {
+                goCari();
+            }
+        }
+    };
+
+    private void gotoLokasi() {
+        EditText lat = (EditText) findViewById(R.id.idLokasiLat);
+        EditText lng = (EditText) findViewById(R.id.idLokasiLng);
+        EditText zoom = (EditText) findViewById(R.id.idZoom);
+
+        Double dbllat = Double.parseDouble(lat.getText().toString());
+        Double dbllng = Double.parseDouble(lng.getText().toString());
+        Float dblzoom = Float.parseFloat(zoom.getText().toString());
+
+        Toast.makeText(this, "Move to Lat:" + dbllat + " Long:" + dbllng, Toast.LENGTH_LONG).show();
+        gotoPeta(dbllat, dbllng, dblzoom);
+    }
+
+
+    private void sembunyikanKeyBoard(View v) {
+        InputMethodManager a = (InputMethodManager)
+                getSystemService(INPUT_METHOD_SERVICE);
+        a.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    private void gotoPeta(Double lat, Double lng, float z) {
+        LatLng Lokasibaru = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().
+                position(Lokasibaru).
+                title("Marker in  " + lat + ":" + lng));
+        mMap.moveCamera(CameraUpdateFactory.
+                newLatLngZoom(Lokasibaru, z));
+
+    }
+
+    private void goCari(){
+        EditText tempat = (EditText) findViewById(R.id.idDaerah);
+        Geocoder g = new Geocoder(getBaseContext());
+        try {
+            List<android.location.Address> daftar =
+                    g.getFromLocationName(tempat.getText().toString(),1);
+            Address alamat = daftar.get(0);
+
+            String nemuAlamat =  alamat.getAddressLine(0);
+            Double lintang = alamat.getLatitude();
+            Double bujur = alamat.getLongitude();
+
+            Toast.makeText(getBaseContext(),"Ketemu " + nemuAlamat,Toast.LENGTH_LONG).show();
+
+            EditText zoom = (EditText) findViewById(R.id.idZoom);
+            Float dblzoom = Float.parseFloat(zoom.getText().toString());
+            Toast.makeText(this,"Move to "+ nemuAlamat +" Lat:" +
+                    lintang + " Long:" +bujur,Toast.LENGTH_LONG).show();
+            gotoPeta(lintang,bujur,dblzoom);
+
+            EditText lat = (EditText) findViewById(R.id.idLokasiLat);
+            EditText lng = (EditText) findViewById(R.id.idLokasiLng);
+
+            lat.setText(lintang.toString());
+            lng.setText(bujur.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
